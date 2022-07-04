35511b42427aeb47c8e3b799528b8d5fa70a65c3
isijolatosin <isijolatosin2210@gmail.com>
2022-07-04 02:24:35 -0400

    second commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import emailjs from 'emailjs-com'
 import Layout from '../components/shared/Layout'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Contact = () => {
 	const [showResponse, setShowResponse] = React.useState(false)
